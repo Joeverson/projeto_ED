2150f0ba9565b1c9ea6d9d10ea2638a3b3af27ad
joeverson <joeverson.barbosa@unisigma.com.br>
2014-05-27 00:24:00 -0300

    alguns bugs, mas esse é o esbolso da lista de carros

--- vias.h
+++ vias.h
@@ -1,11 +1,36 @@
-#include <alloc.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef char tipo_carro; // cria um tipo de variavel chamado carro onde ficara uma string
 
 typedef struct veiculo_h{ // esse struct é para os veiculos que viram na horizontal
 	tipo_carro car[30]; // criando um vetor de char(string) do tipo tpo_carro
-	struct veiculo *next; // ponteiro que aponta para frente
-	struct veiculo *prev; // pnteiro que apnta traz
+	struct veiculo_h* next; // ponteiro que aponta para frente
+	struct veiculo_h* prev; // pnteiro que apnta traz
 }via_h;
 
-typedef *via_h pista_h;
+typedef via_h* pista_h; // ponteiro para o tipo via_h
+
+
+void criar(pista_h *c){ // inicializando a lista duplamente encadeada
+	*c = NULL;
+}
+
+int inserir_puro(pista_h *c, tipo_carro veiculo){
+	pista_h novo;
+	
+	novo = (pista_h) malloc(sizeof(via_h)); // rezervando espaço na memoria para a nova estrutura
+	if(novo == NULL) return 0;
+	novo->car[30] = veiculo;
+	novo->prev = NULL;
+	novo->next = NULL;
+	*c = novo;
+	return 1;
+	
+}
+
+void view(pista_h c){
+	while(c->next != NULL){
+		printf("%s\n",c->car);
+	}
+}
